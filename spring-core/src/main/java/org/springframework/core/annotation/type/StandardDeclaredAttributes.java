4e80874684294aa4d9a08f05efe7609fbf40e970
Phillip Webb <pwebb@pivotal.io>
2019-01-28 17:07:24 -0800

    Dont use map for attributes

--- spring-core/src/main/java/org/springframework/core/annotation/type/StandardDeclaredAttributes.java
+++ spring-core/src/main/java/org/springframework/core/annotation/type/StandardDeclaredAttributes.java
@@ -18,9 +18,10 @@ package org.springframework.core.annotation.type;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.AbstractSet;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.util.Assert;
@@ -36,34 +37,13 @@ public class StandardDeclaredAttributes extends AbstractDeclaredAttributes {
 
 	private final Annotation annotation;
 
-	private final Map<String, Method> attributeMethods;
+	private volatile Method[] attributeMethods;
+
+	private final Names names = new Names();
 
 	StandardDeclaredAttributes(Annotation annotation) {
 		Assert.notNull(annotation, "Annotation must not be null");
 		this.annotation = annotation;
-		this.attributeMethods = getAttributeMethods(annotation.annotationType());
-	}
-
-	private Map<String, Method> getAttributeMethods(
-			Class<? extends Annotation> annotationType) {
-		Method[] candidates = annotationType.getDeclaredMethods();
-		Map<String, Method> attributeMethods = new LinkedHashMap<>(candidates.length);
-		for (Method candidate : candidates) {
-			if (isAttributeMethod(candidate)) {
-				assertAttibuteMethodIsCallable(candidate);
-				attributeMethods.put(candidate.getName(), candidate);
-			}
-		}
-		return Collections.unmodifiableMap(attributeMethods);
-	}
-
-	private void assertAttibuteMethodIsCallable(Method candidate) {
-		candidate.setAccessible(true);
-		get(candidate);
-	}
-
-	private boolean isAttributeMethod(Method method) {
-		return (method.getParameterCount() == 0 && method.getReturnType() != void.class);
 	}
 
 	/**
@@ -76,26 +56,72 @@ public class StandardDeclaredAttributes extends AbstractDeclaredAttributes {
 
 	@Override
 	public Set<String> names() {
-		return this.attributeMethods.keySet();
+		return this.names;
 	}
 
 	@Override
 	public Object get(String name) {
-		Method method = this.attributeMethods.get(name);
-		if (method == null) {
-			return null;
+		for (Method attributeMethod : getAttributeMethods()) {
+			if (attributeMethod != null && attributeMethod.getName().equals(name)) {
+				return get(attributeMethod);
+			}
 		}
-		return get(method);
+		return null;
+	}
+
+	private Method[] getAttributeMethods() {
+		Method[] result = this.attributeMethods;
+		if (result != null) {
+			return result;
+		}
+		result = annotation.annotationType().getDeclaredMethods();
+		for (int i = 0; i < result.length; i++) {
+			if (isAttributeMethod(result[i])) {
+				result[i].setAccessible(true);
+			}
+			else {
+				result[i] = null;
+			}
+		}
+		this.attributeMethods = result;
+		return result;
+	}
+
+	private boolean isAttributeMethod(Method method) {
+		return (method.getParameterCount() == 0 && method.getReturnType() != void.class);
 	}
 
 	private Object get(Method method) {
 		try {
+			method.setAccessible(true);
 			return convert(method.invoke(this.annotation));
 		}
 		catch (Throwable ex) {
 			throw new IllegalStateException(
 					"Could not obtain annotation attribute value for " + method, ex);
 		}
+	}
+
+	/**
+	 * Set implementation used to return the attribute names.
+	 */
+	private class Names extends AbstractSet<String> {
+
+		@Override
+		public Iterator<String> iterator() {
+			return Arrays.stream(getAttributeMethods()).filter(Objects::nonNull).map(
+					Method::getName).iterator();
+		}
+
+		@Override
+		public int size() {
+			int result = 0;
+			for (Method method : getAttributeMethods()) {
+				result += method != null ? 1 : 0;
+			}
+			return result;
+		}
+
 	}
 
 }
